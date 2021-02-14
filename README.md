d0cc54c4fd9fadeb1de687ff02f2b98f7ec8fd9f
dhruv5harma <47002309+dhruv5harma@users.noreply.github.com>
2021-02-14 22:29:42 +0530

    Revert "Initial commit"
    This reverts commit f4c79729b76197615b3d8dfd5ac506c95a165969.

--- README.md
+++
@@ -1,2 +0,0 @@
-# DemoProjects
- This Repo is for DemoProjects
--- src/com/dhruv/HelloWorld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.dhruv.HelloWorld;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World, This is my first Program. Nice to meet you");
-	}
-
-}
