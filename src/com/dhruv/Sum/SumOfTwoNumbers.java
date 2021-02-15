24c157823a5d97204e716d0a86e6cced95a2345c
dhruv5harma <47002309+dhruv5harma@users.noreply.github.com>
2021-02-15 23:09:34 +0530

    Create SumOfTwoNumbers.java
    Program is to calculate sum of two numbers.

---
+++ src/com/dhruv/Sum/SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.dhruv.Sum;
+
+public class SumOfTwoNumbers {
+	public static void main(String[] args) {
+		System.out.println("Calculating Sum of 32 and 43");
+		int a=32;
+		int b=43;
+		int sum=a+b;
+		System.out.println("Sum of numbers is "+sum);
+	}
+}
