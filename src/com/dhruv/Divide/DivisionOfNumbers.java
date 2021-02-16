32c483b3c3edac225139ab771e6acee95cf4af7f
dhruv5harma <47002309+dhruv5harma@users.noreply.github.com>
2021-02-16 21:25:39 +0530

    This Program will divide a number by another

---
+++ src/com/dhruv/Divide/DivisionOfNumbers.java
@@ -0,0 +1,14 @@
+package com.dhruv.Divide;
+
+public class DivisionOfNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Program will divide 2nd Number by first");
+		int a = 12;
+		int b = 298;
+		float divident = b / a;
+		System.out.println("The result of Division of " + b + " by " + a + " is " + divident);
+
+	}
+
+}
