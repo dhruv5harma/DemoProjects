194fe5d69b23f5d7b92ed405f777f962c64e1964
dhruv5harma <47002309+dhruv5harma@users.noreply.github.com>
2021-02-18 00:07:11 +0530

    Create OprationsOfNumbers.java

---
+++ src/com/dhruv/Oprations/OprationsOfNumbers.java
@@ -0,0 +1,14 @@
+package com.dhruv.Oprations;
+
+public class OprationsOfNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Java program to print the result of the following operations : +, -, /, *, %");
+		System.out.println("Addition of two numbers, 12 + 7 is = "+(7+12));
+		System.out.println("Subtraction of two numbers, 12 - 7 is = "+(12-7));
+		System.out.println("Division of two numbers, 12 / 7 is = "+(12/7));
+		System.out.println("Multiplication of two numbers, 12 * 7 is = "+(7*12));
+		System.out.println("Reminder of Divison of two numbers, 12 % 7 is = "+(12%7));
+	}
+
+}
